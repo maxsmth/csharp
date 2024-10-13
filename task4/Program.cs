5175c49667b4e2a912eb40a9193d718ae3d3ad63
maxsmth <proverbium@yahoo.com>
2024-10-13 18:26:49 +0500

    task4

---
+++ task4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Введите x: ");
+            int x = int.Parse(Console.ReadLine());
+
+            double res = Func(x);
+            Console.WriteLine("f(x) = " + res);
+            Console.WriteLine("Для выхода нажмите любую клавишу");
+            Console.ReadKey();
+        }
+
+        static double Func(int x)
+        {
+            return (Math.Sqrt(x + 1) + Math.Sqrt(x - 1)) / Math.Sqrt(x);
+        }
+    }
+}
