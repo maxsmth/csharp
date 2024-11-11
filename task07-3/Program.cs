51014d5e76a670ea3f456675abfda00ab8c990e4
maxsmth <proverbium@yahoo.com>
2024-11-11 13:01:52 +0500

    task8

---
+++ task07-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task07_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите позицию белого слона");
+            var whiteBishop = Console.ReadLine();
+            int column, row;
+            DecodePosition(whiteBishop, out column, out raw);
+
+            Console.ReadLine();
+        }
+
+        static void DecodePosition(string position, out int column, out int raw)
+        {
+            column = (int)position[0] - 0x60;
+            row = int.Parse(position[1].ToString());
+        }
+    }
+}
---
+++ task8/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите значение x: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double result = MyFunction(x);
+            Console.WriteLine($"f({x}) = {result}");
+            Console.ReadKey();
+        }
+
+        static double MyFunction(double x)
+        {
+            if (x < -4)
+            {
+                return 46;
+            }
+            else if (x > 2)
+            {
+                return 10;
+            }
+            else
+            {
+                return 3 * x * x - 2;
+            }
+        }
+    }
+}
